d8994aada4f4f319671db64392b81082bc3214da
k-okada <devnull@localhost>
2012-08-10 10:39:45 +0900

    geometry->filname needs file://

--- collada_parser/src/collada_parser.cpp
+++ collada_parser/src/collada_parser.cpp
@@ -1291,7 +1291,7 @@ protected:
         }
         close(fd);
         _listTempFilenames.push_back(boost::shared_ptr<UnlinkFilename>(new UnlinkFilename(geometry->filename)));
-        //geometry->filename = std::string("file://") + geometry->filename;
+        geometry->filename = std::string("file://") + geometry->filename;
         return geometry;
     }
 
