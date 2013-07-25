acced6ec34660d0f0b0176308f567979b7305e01
William Woodall <william@osrfoundation.org>
2013-07-24 23:32:40 -0700

    resource_retriever: install python package using setup.py

---
+++ resource_retriever/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup()
+d['packages'] = ['resource_retriever']
+d['scripts'] = []
+d['package_dir'] = {'': 'src'}
+
+setup(**d)
