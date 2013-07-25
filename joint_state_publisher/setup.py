d64167967fee6720c1d2debe8111577d69c73789
William Woodall <william@osrfoundation.org>
2013-07-24 23:32:14 -0700

    joint_state_publisher: do not install script to global bin
    Also clean up no longer required setup.py

--- joint_state_publisher/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup()
-d['packages'] = []
-d['scripts'] = ['joint_state_publisher/joint_state_publisher']
-d['package_dir'] = {}
-
-setup(**d)
