210654ace651646b87c2849e96ef8f391bd76ad1
William Woodall <william@osrfoundation.org>
2015-04-21 18:16:28 -0700

    move resource_retriever to its own repository

--- resource_retriever/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup()
-d['packages'] = ['resource_retriever']
-d['scripts'] = []
-d['package_dir'] = {'': 'src'}
-
-setup(**d)
