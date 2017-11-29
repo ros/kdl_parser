ca04c0f4803190ab50cf29f23c3ba66868697825
Chris Lalancette <clalancette@openrobotics.org>
2017-11-29 19:28:59 +0000

    Style fixes (these can go to upstream kdl_parser)
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- kdl_parser/include/kdl_parser/kdl_parser.hpp
+++ kdl_parser/include/kdl_parser/kdl_parser.hpp
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2008, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -34,50 +34,51 @@
 
 /* Author: Wim Meeussen */
 
-#ifndef KDL_PARSER_H
-#define KDL_PARSER_H
+#ifndef KDL_PARSER__KDL_PARSER_HPP_
+#define KDL_PARSER__KDL_PARSER_HPP_
 
 #include <kdl/tree.hpp>
 #include <string>
 #include <urdf_model/model.h>
-#include <tinyxml.h>
+#include <tinyxml.h>  // NOLINT
 
-namespace kdl_parser{
+namespace kdl_parser
+{
 
 /** Constructs a KDL tree from a file, given the file name
  * \param file The filename from where to read the xml
  * \param tree The resulting KDL Tree
  * returns true on success, false on failure
  */
-bool treeFromFile(const std::string& file, KDL::Tree& tree);
+bool treeFromFile(const std::string & file, KDL::Tree & tree);
 
 /** Constructs a KDL tree from the parameter server, given the parameter name
  * \param param the name of the parameter on the parameter server
  * \param tree The resulting KDL Tree
  * returns true on success, false on failure
  */
-bool treeFromParam(const std::string& param, KDL::Tree& tree);
+bool treeFromParam(const std::string & param, KDL::Tree & tree);
 
 /** Constructs a KDL tree from a string containing xml
  * \param xml A string containting the xml description of the robot
  * \param tree The resulting KDL Tree
  * returns true on success, false on failure
  */
-bool treeFromString(const std::string& xml, KDL::Tree& tree);
+bool treeFromString(const std::string & xml, KDL::Tree & tree);
 
 /** Constructs a KDL tree from a TiXmlDocument
  * \param xml_doc The TiXmlDocument containting the xml description of the robot
  * \param tree The resulting KDL Tree
  * returns true on success, false on failure
  */
-bool treeFromXml(TiXmlDocument *xml_doc, KDL::Tree& tree);
+bool treeFromXml(TiXmlDocument * xml_doc, KDL::Tree & tree);
 
 /** Constructs a KDL tree from a URDF robot model
  * \param robot_model The URDF robot model
  * \param tree The resulting KDL Tree
  * returns true on success, false on failure
  */
-bool treeFromUrdfModel(const urdf::ModelInterface& robot_model, KDL::Tree& tree);
-}
+bool treeFromUrdfModel(const urdf::ModelInterface & robot_model, KDL::Tree & tree);
+}  // namespace kdl_parser
 
-#endif
+#endif  // KDL_PARSER__KDL_PARSER_HPP_
