5ae05764dac3081faa2e0e03a5eb3549ca3727cf
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2020-04-01 23:39:11 +0200

    noetic release (#36)
    * Bump CMake version to avoid CMP0048 warning
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Use setuptools instead of distutils
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Add buildtool depend on setuptools
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Remove unnecessary option 'package_dir'
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    Co-authored-by: Shane Loretz <sloretz@osrfoundation.org>

--- kdl_parser_py/setup.py
+++ kdl_parser_py/setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['kdl_parser_py'],
-   package_dir={'': ''}
+   packages=['kdl_parser_py']
 )
 
 setup(**d)
