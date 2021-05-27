ef2ff8adb806e18a46064cfd509d46e33175d6c5
Adrian Zwiener <a.zwiener@rethinkrobotics.com>
2021-05-27 11:36:56 +0200

    Port kdl_parser_py to ROS 2

--- kdl_parser_py/test/test_kdl_parser.py
+++ kdl_parser_py/test/test_kdl_parser.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
+import os
 
 import kdl_parser_py.urdf
 import unittest
-
-import rostest
+from ament_index_python.packages import get_package_share_directory
 
 PKG = "kdl_parser_py"
 NAME = "test_kdl_parser"
 
 class TestKdlParser(unittest.TestCase):
     def runTest(self):
-        filename = None
-        if (sys.argv > 1):
-            filename = sys.argv[1]
-        else:
-            self.fail("Expected filename!")
+        # filename = None
+        # if (sys.argv > 1):
+        #     filename = sys.argv[1]
+        # else:
+        #     self.fail("Expected filename!")
+        package_dir = get_package_share_directory(PKG)
+
+        filename  = os.path.join(package_dir, "test.urdf")
         (ok, tree) = kdl_parser_py.urdf.treeFromFile(filename)
         self.assertTrue(ok)
         # KDL doesn't count fixed joints (since they aren't kinematic)
@@ -35,4 +52,4 @@ class TestKdlParser(unittest.TestCase):
         self.assertAlmostEqual(inertia.getCOG().z(), 3.0)
 
 if __name__ == '__main__':
-    rostest.run(PKG, NAME, TestKdlParser, sys.argv)
+    unittest.main()
